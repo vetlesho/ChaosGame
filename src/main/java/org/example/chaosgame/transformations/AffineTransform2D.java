8539c4ad1229e7939d6d34fbc6dda8cb74b42bcf
Edvard <edvardee@stud.ntnu.no>
2024-02-21 14:52:46 +0100

    Implementation of variables and methods in transformations classes

--- src/main/java/org/example/chaosgame/transformations/AffineTransform2D.java
+++ src/main/java/org/example/chaosgame/transformations/AffineTransform2D.java
@@ -1,4 +1,19 @@
 package org.example.chaosgame.transformations;
 
+import org.example.chaosgame.linalg.Matrix2x2;
+import org.example.chaosgame.linalg.Vector2D;
+
 public class AffineTransform2D implements Transform2D{
+  private final Matrix2x2 matrix;
+  private final Vector2D vector;
+
+  public AffineTransform2D(Matrix2x2 matrix, Vector2D vector) {
+    this.matrix = matrix;
+    this.vector = vector;
+  }
+
+  @Override
+  public Vector2D transform(Vector2D point) {
+    return matrix.multiply(point).add(vector);
+  }
 }
