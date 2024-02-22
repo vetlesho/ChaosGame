787d33388541d7fc56b79f093631badbc5d4f93e
vetlesho <vetlesho@stud.ntnu.no>
2024-02-22 14:43:05 +0100

    Added more tests and javadoc for Complex, and Vector2D.

--- src/main/java/org/example/chaosgame/linalg/Vector2D.java
+++ src/main/java/org/example/chaosgame/linalg/Vector2D.java
@@ -1,11 +1,17 @@
 package org.example.chaosgame.linalg;
 
+/**
+ * Class for 2D vectors.
+ * Vectors are represented by an x-coordinate and a y-coordinate.
+ * The class contains methods for adding and subtracting vectors.
+ */
 public class Vector2D {
   private final double x;
   private final double y;
 
   /**
-   * Constructor for Vector2D
+   * Constructor for Vector2D.
+   *
    * @param x x-coordinate
    * @param y y-coordinate
    */
@@ -23,7 +29,8 @@ public class Vector2D {
   }
 
   /**
-   * Add two vectors together
+   * Add two vectors together.
+   *
    * @param other the other vector
    * @return the sum of the two vectors
    */
@@ -34,7 +41,8 @@ public class Vector2D {
   }
 
   /**
-   * Subtract one vector from another
+   * Subtract one vector from another.
+   *
    * @param other the other vector
    * @return the difference of the two vectors
    */
@@ -43,6 +51,4 @@ public class Vector2D {
             x - other.x,
             y - other.y);
   }
-
-
 }
