c5431fb95efeb2e5e9c24a0f6c3a3c5c6ec7ff22
vetlesho <vetlesho@stud.ntnu.no>
2024-02-22 11:35:36 +0100

    Edited testclass and javadoc for Vector2D class

--- src/main/java/org/example/chaosgame/linalg/Vector2D.java
+++ src/main/java/org/example/chaosgame/linalg/Vector2D.java
@@ -4,6 +4,11 @@ public class Vector2D {
   private final double x;
   private final double y;
 
+  /**
+   * Constructor for Vector2D
+   * @param x x-coordinate
+   * @param y y-coordinate
+   */
   public Vector2D(double x, double y) {
     this.x = x;
     this.y = y;
@@ -17,12 +22,26 @@ public class Vector2D {
     return y;
   }
 
+  /**
+   * Add two vectors together
+   * @param other the other vector
+   * @return the sum of the two vectors
+   */
   public Vector2D add(Vector2D other) {
-    return new Vector2D(x + other.x, y + other.y);
+    return new Vector2D(
+            x + other.x,
+            y + other.y);
   }
 
+  /**
+   * Subtract one vector from another
+   * @param other the other vector
+   * @return the difference of the two vectors
+   */
   public Vector2D subtract(Vector2D other) {
-    return new Vector2D(x - other.x, y - other.y);
+    return new Vector2D(
+            x - other.x,
+            y - other.y);
   }
 
 
