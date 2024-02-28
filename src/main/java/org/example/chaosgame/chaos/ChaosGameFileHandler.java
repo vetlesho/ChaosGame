fd3dabb18198ee24fb8196721f88c00c7a2f953f
Edvard <edvardee@stud.ntnu.no>
2024-02-28 11:17:10 +0100

    Add classes ChaosGame and ChaosGameFileHandler
    Implements methods and fields from projects class diagram description.
    ChaosGameFileHandler methods and constructors does not have a body as task
    description was unclear. ChaosGame logic is not yet completed.

---
+++ src/main/java/org/example/chaosgame/chaos/ChaosGameFileHandler.java
@@ -0,0 +1,10 @@
+package org.example.chaosgame.chaos;
+
+public class ChaosGameFileHandler {
+  public ChaosGameDescription readFromFile(String path){
+    return null;
+  }
+  public void writeToFile(String path){
+
+  }
+}
