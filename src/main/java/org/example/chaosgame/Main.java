1256033e9eea18931b2bce891aee8279db8eca74
Edvard <edvardee@stud.ntnu.no>
2024-02-29 10:34:55 +0100

    Implement boundaries to putPixel method
    Prevent IndexOutOfBoundsException by setting boundaries for what indices are added to the canvas.
    Add test in main that prints a Julia set
    by setting boundaries for what indices are added to the canvas.
    Add test in main that prints a Julia set

--- src/main/java/org/example/chaosgame/Main.java
+++ src/main/java/org/example/chaosgame/Main.java
@@ -3,15 +3,17 @@ package org.example.chaosgame;
 import org.example.chaosgame.chaos.ChaosCanvas;
 import org.example.chaosgame.chaos.ChaosGame;
 import org.example.chaosgame.chaos.ChaosGameDescription;
+import org.example.chaosgame.linalg.Complex;
 import org.example.chaosgame.linalg.Vector2D;
 import org.example.chaosgame.transformations.AffineTransform2D;
+import org.example.chaosgame.transformations.JuliaTransform;
 import org.example.chaosgame.transformations.Transform2D;
 import org.example.chaosgame.linalg.Matrix2x2;
 import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-
+    JuliaTransform juliaTransform = new JuliaTransform(new Complex(-0.84, -0.2), 1);
     AffineTransform2D transform = new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
             new Vector2D(0, 0));
     AffineTransform2D transform2 = new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
@@ -19,11 +21,12 @@ public class Main {
     AffineTransform2D transform3 = new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
             new Vector2D(0.25, 0.5));
     List<Transform2D> transforms = List.of(transform, transform2, transform3);
+    List<Transform2D> transforms2 = List.of(juliaTransform);
 
     ChaosGameDescription description = new ChaosGameDescription(
             new Vector2D(0, 0),
             new Vector2D(1, 1),
-            transforms
+            transforms2
     );
 
     ChaosGame game = new ChaosGame(description, 100, 49);
