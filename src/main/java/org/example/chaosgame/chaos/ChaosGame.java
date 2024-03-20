3f78f4e96dc46ffb9d56d7b197c6ce6fb5760722
vetlesho <vetlesho@stud.ntnu.no>
2024-03-20 15:39:00 +0100

    Created runsteps method for barnsley
    Created a new runsteps method with calculated probabilities for the barnsley transformation.
    Added hue for better visualization of the transformations.

--- src/main/java/org/example/chaosgame/chaos/ChaosGame.java
+++ src/main/java/org/example/chaosgame/chaos/ChaosGame.java
@@ -7,7 +7,9 @@ import javafx.scene.paint.Color;
 import org.example.chaosgame.linalg.Matrix2x2;
 import org.example.chaosgame.linalg.Vector2D;
 import org.example.chaosgame.transformations.AffineTransform2D;
+import org.example.chaosgame.transformations.Transform2D;
 
+import java.util.List;
 import java.util.Random;
 
 public class ChaosGame {
@@ -24,10 +26,28 @@ public class ChaosGame {
   public ChaosCanvas getCanvas() {
     return canvas;
   }
+
+
   public void runSteps (int steps){
     for (int i = 0; i < steps; i++){
       int transformIndex = random.nextInt(description.getTransforms().size());
       currentPoint = description.getTransforms().get(transformIndex).transform(currentPoint);
+      canvas.putPixel(currentPoint);
+    }
+  }
+
+  public void runStepsBarnsley (int steps){
+    for (int i = 0; i < steps; i++){
+      int test = random.nextInt(100);
+      if (test < 1){
+        currentPoint = description.getTransforms().getFirst().transform(currentPoint);
+      } else if (test < 86){
+        currentPoint = description.getTransforms().get(1).transform(currentPoint);
+      } else if (test < 93){
+        currentPoint = description.getTransforms().get(2).transform(currentPoint);
+      } else {
+        currentPoint = description.getTransforms().get(3).transform(currentPoint);
+      }
       canvas.putPixel(currentPoint);
     }
   }
