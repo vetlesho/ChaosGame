c9dd91b20a976b54465e467588b54d6a357f8043
Edvard <edvardee@stud.ntnu.no>
2024-05-16 11:29:14 +0200

    Remove slider

--- src/main/java/org/example/chaosgame/view/ExplorePage.java
+++ src/main/java/org/example/chaosgame/view/ExplorePage.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 import org.example.chaosgame.controller.ExploreGameController;
-import org.example.chaosgame.view.components.JuliaSlider;
 import org.example.chaosgame.view.components.SideBar;
 
 public class ExplorePage extends GamePage {
@@ -17,24 +16,8 @@ public class ExplorePage extends GamePage {
     this.sidebar = new SideBar(exploreGameController);
     Button homeButton = createHomeButton(event -> exploreGameController.homeButtonClicked());
     exploreGameController.setCanvas(gc.getCanvas());
-    Slider juliaSliderReal = new JuliaSlider();
-    juliaSliderReal.valueProperty().addListener((observable, oldValue, newValue) -> {
-      exploreGameController.updateJuliaValue(newValue.doubleValue(), true);
-    });
-    VBox realBox = new VBox(juliaSliderReal);
-    realBox.setSpacing(10);
-    realBox.setAlignment(Pos.BOTTOM_CENTER);
 
-    Slider juliaSliderImaginary = new JuliaSlider();
-    juliaSliderImaginary.valueProperty().addListener((observable, oldValue, newValue) -> {
-      exploreGameController.updateJuliaValue(newValue.doubleValue(), false);
-    });
-    juliaSliderImaginary.setOrientation(Orientation.VERTICAL);
-    VBox imgBox = new VBox(juliaSliderImaginary);
-    imgBox.setSpacing(10);
-    imgBox.setAlignment(Pos.CENTER_LEFT);
-    this.getChildren().addAll(gc.getCanvas(), sidebar, homeButton, realBox, imgBox);
-    this.setStyle("-fx-background-color: black;");
+    this.getChildren().addAll(gc.getCanvas(), sidebar, homeButton);
     this.setOnScroll(event -> {
       try{
       exploreGameController.onScroll(event);
